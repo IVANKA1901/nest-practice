298363ba605a018e08b996ca2b26d7ae854c1890
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-27 16:08:49 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import {  } from "@nes";
 import { MongooseModule } from '@nestjs/mongoose';
 
-const environment = process.env.NODE_ENV || "development"
+const environment = process.env.NODE_ENV || 'development';
 
 @Module({
-  imports: [UserModule,
-    ConfigModule.forRoot{{
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
       envFilePath: `.env.${environment}`,
-      isGlobal: true
-    }},
-  MongooseModule.forRoot{
-    
-
-  }
-  
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.DATABASE_URL,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
-},
-)
+})
 export class AppModule {}
