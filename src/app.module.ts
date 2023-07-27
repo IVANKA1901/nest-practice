73baa4b6176d07bdde6a950a35b1cce2cefd0a9a
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-27 15:27:09 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import {  } from "@nes";
+import { MongooseModule } from '@nestjs/mongoose';
+
+const environment = process.env.NODE_ENV || "development"
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    ConfigModule.forRoot{{
+      envFilePath: `.env.${environment}`,
+      isGlobal: true
+    }},
+  MongooseModule.forRoot{
+    
+
+  }
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
-})
+},
+)
 export class AppModule {}
