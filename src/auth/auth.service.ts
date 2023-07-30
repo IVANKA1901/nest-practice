30b883d131ab6e48b11bd790dc1fb50b2416f6bc
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-30 17:19:27 +0200

    add verify token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { SignOptions } from 'jsonwebtoken';
+
+import { TokenService } from 'src/token/token.service';
+// import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+    private readonly tokenService: TokenService,
+  ) {}
+
+  //   signUp(user: CreateUserDto) {}
+
+  //   signIn(email, password) {}
+
+  private async generateToken(data, options?: SignOptions): Promise<string> {
+    return this.jwtService.sign(data, options);
+  }
+
+  private async verifyToken(token): Promise<any> {
+    try {
+      const data = this.jwtService.verify(token);
+      const tokenExist = await this.tokenService.exists(data._id, token);
+
+      if (tokenExist) {
+        return data;
+      }
+      throw new UnauthorizedException();
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+}
