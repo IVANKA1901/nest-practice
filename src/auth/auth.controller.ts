4e9f81973bfafbd5f390078e29ac664a1e117125
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-31 13:14:50 +0200

    add signuo/confirm for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  ValidationPipe,
+  Get,
+  Query,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { ConfirmAccountDto } from './dto/confirm-account.dto';
+
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signUp')
+  async signup(@Body(ValidationPipe) user: CreateUserDto): Promise<boolean> {
+    return this.authService.signUp(user);
+  }
+
+  @Get('/confirm')
+  async confirm(@Query(ValidationPipe) query: ConfirmAccountDto) {
+    await this.authService.confirm(query.token);
+    return true;
+  }
+}
