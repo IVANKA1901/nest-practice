42446376c9d3717aaed39cc6370de40a56ee9e2e
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-08-01 16:16:30 +0200

    Create get-user.decorator.ts

---
+++ src/components/decorators/get-user.decorator.ts
@@ -0,0 +1,5 @@
+import { createParamDecorator } from '@nestjs/common';
+
+import { IUser } from 'src/user/interfaces/user.interface';
+
+export const GetUser = createParamDecorator((data, req): IUser => req.user);
